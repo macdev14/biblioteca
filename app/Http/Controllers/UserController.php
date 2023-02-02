ec575ca89d94343fc622f0a5e5ee0c571dbf85bd
lauro <lauropimentel12@gmail.com>
2023-02-02 10:01:11 -0300

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Http\Requests\StoreUserRequest;
 
 class UserController extends Controller
 {
@@ -13,25 +14,13 @@ class UserController extends Controller
         return view('users.register');
     }
 
-    public function store(){
-        $formFields = request()->validate([
-            'name'=>['required', 'min:3'],
-            'email'=>['required', 'email', Rule::unique('users','email')],
-            'password'=>['required', 'confirmed', 'min:6'],
-            // 'password_confirmation'=>'required',
-        ],
-        ['name.required'=>'Favor inserir nome.', 
-        'email.required'=>'Favor inserir email.',
-        'password.required'=> 'Favor inserir senha.',
-        'password.confirmed' => 'Favor confirmar senha.',
-        'password.min:6' => 'A senha deve ter pelo menos 6 caracteres.',
-        // 'password_confirmation.required'=> 'Favor confirmar senha'
-        ]);
+    public function store(StoreUserRequest $request){
+        $formFields = $request->validated();
 
         $formFields['password'] = bcrypt($formFields['password']);
 
         $user = User::create($formFields);
-        $user->assignRole('Usuario');
+        $user->assignRole('usuario');
         auth()->login($user);
         return redirect('/')->with('message', 'Usuário cadastrado e logado.');
     }
