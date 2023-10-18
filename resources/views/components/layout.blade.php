a54fb0b60456c46da31d58fe34d0e19a3e717946
lauro <lauropimentel12@gmail.com>
2023-10-18 10:47:49 -0300

    bg color

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
 
 <body>
   <header>
-    <nav class="navbar navbar-expand-lg bg-primary-subtle">
+    <nav class="navbar navbar-expand-lg" style="background-color:#77CEFF">
         <div class="container-fluid">
           <a class="navbar-brand" href="{{url('/')}}">MedLobby</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
