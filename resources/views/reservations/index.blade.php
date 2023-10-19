f902ffb9375fdde7db8cd17e96ee66dbc64a0062
lauro <lauropimentel12@gmail.com>
2023-10-18 22:37:57 -0300

    autor-publicacao

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -2,32 +2,31 @@
 <x-layout>
 
     <div class="row justify-content-center align-items-center g-2">
-        <h2 class="">Livro(s) Reservados</h2>
+        <h2 class="">Publicação e seu seguinte Autor</h2>
     </div>
-    
+
     <div class="row align-items-center mt-5">
         @if($reservations->isEmpty())
-        <h3 class="">Nenhum Livro Reservado</h3>
+        <h3 class="">Nenhuma Publicação com seu seguinte Autor</h3>
         @else
     @foreach($reservations as $reservation)
-    
-       
+
+
           <x-bookcard :book="$reservation->book"/>
-    
-    
-        
-        
-    
-    @endforeach 
+
+
+
+
+
+    @endforeach
     @endif
 
-   
-    
+
+
     </div>
     <div class="row mt-2">
-    
-    
-    
+
+
+
     </div>
     </x-layout>
-    
